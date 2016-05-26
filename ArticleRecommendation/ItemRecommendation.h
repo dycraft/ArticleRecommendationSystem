5e607d4a9af824fc619deec4c2c1e4b0d95a56ae
Admin <Admin@DY>
2016-05-27 01:37:11 +0800

    Let's start!

---
+++ ArticleRecommendation/ItemRecommendation.h
@@ -0,0 +1,6 @@
+#pragma once
+//implement by Fan Kang-Jing
+
+#include "stdafx.h"
+#include "User.h"
+#include "Article.h"
---
+++ ArticleRecommendation/PersonalizedRecommendation.h
@@ -0,0 +1,6 @@
+#pragma once
+//implement by Yang Jing
+
+#include "stdafx.h"
+#include "User.h"
+#include "Article.h"
